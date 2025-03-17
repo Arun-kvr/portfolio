0c8327aa474ac930c345b8011434b02d829eb055
Arun-kvr <141840990+Arun-kvr@users.noreply.github.com>
2025-03-17 21:02:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+const modeToggle = document.getElementById('modeToggle');
+const body = document.body;
+
+modeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+    body.classList.toggle('light-mode');
+});
